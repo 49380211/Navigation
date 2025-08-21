33decda3b5c1fee1a17c1e66a4e73093091dcf42
49380211 <49380211@est.ort.edu.ar>
2025-08-21 10:40:38 -0300

    dea

--- Assets/Scripts/AgentScript.cs
+++ Assets/Scripts/AgentScript.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AgentNavigationScript : MonoBehaviour
+public class AgentScript : MonoBehaviour
 {
-    [SerializeField] NavMeshAgent agent;
+    NavMeshAgent agent;
     [SerializeField] Transform targetTR;
     [SerializeField] Animator anim;
+    [SerializeField] float velocity;
 
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-    agent.destination = targetTR.position;
-    anim.setFloat("Speed", agent.velocity.magnitude)
+        agent.destination = targetTR.position;
+        velocity = agent.velocity.magnitude;
+        anim.SetFloat("Speed",velocity);
     }
 }
